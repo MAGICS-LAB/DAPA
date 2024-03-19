6c8fee5ee74954691c7ed35c4fba10533704022e
robin luo <luohaozh@gmail.com>
2024-03-19 05:24:04 +0000

    Updpate analysis.py. Delete temp.py

--- plugin_aligner/analyze.py
+++ plugin_aligner/analyze.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import os
 import sys
+import argparse
 
-def count_results_in_csv(directory):
+def count_results_in_csv(args):
     count_1 = 0
     count_0 = 0
     count_err = 0
-    for filename in os.listdir(directory):
+    for filename in os.listdir(args.directory_path):
         if filename.endswith('.csv'):
-            file_path = os.path.join(directory, filename)
+            file_path = os.path.join(args.directory_path, filename)
             try:
                 df = pd.read_csv(file_path)
                 if 'Success' in df.columns:
@@ -24,12 +25,12 @@ def count_results_in_csv(directory):
     return count_1, count_0, count_err
 
 if __name__ == "__main__":
-    # Default directory path
-    default_directory = 'Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer'
+    parser = argparse.ArgumentParser(description='Analysis the safe rate of the model response.')
+    parser.add_argument('directory_path', default='Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer',
+                        help='Result directory path to analysis the safe rate of the response (default: Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer)')
+    args = parser.parse_args()
 
-    # Use the provided directory path if given, otherwise use the default
-    directory_path = sys.argv[1] if len(sys.argv) > 1 else default_directory
-    count_1, count_0, count_err = count_results_in_csv(directory_path)
+    count_1, count_0, count_err = count_results_in_csv(args)
     total_responses = count_0 + count_1 + count_err
     percentage_safe = (count_0 / total_responses * 100) if total_responses > 0 else 0
     print(f"Total Responses: {total_responses}")
