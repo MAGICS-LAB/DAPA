3bfec1319038373ce75cca0fd301c16c94c28d73
robin <luohaozh@gmail.com>
2024-03-21 19:24:55 -0500

    Updated analyze.py

--- plugin_aligner/analyze.py
+++ plugin_aligner/analyze.py
@@ -2,14 +2,19 @@ import pandas as pd
 import os
 import sys
 import argparse
+import re
+
 
 def count_results_in_csv(args):
     count_1 = 0
     count_0 = 0
     count_err = 0
+    unsafe_list = []
     for filename in os.listdir(args.directory_path):
         if filename.endswith('.csv'):
             file_path = os.path.join(args.directory_path, filename)
+            match = re.search(r'\d+', filename)
+            question_idx = int(match.group())
             try:
                 df = pd.read_csv(file_path)
                 if 'Success' in df.columns:
@@ -17,23 +22,29 @@ def count_results_in_csv(args):
                         count_0 += 1
                     elif df['Success'].sum() == 1:
                         count_1 += 1
+                        unsafe_list.append(question_idx)
                     else:
                         count_err += 1
             except Exception as e:
                 print(f"Error reading {filename}: {e}")
 
-    return count_1, count_0, count_err
+    return count_1, count_0, count_err, unsafe_list
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analysis the safe rate of the model response.')
-    parser.add_argument('directory_path', default='Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer',
+    parser.add_argument('--directory_path', type=str, default='../Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer',
                         help='Result directory path to analysis the safe rate of the response (default: Results2/meta-llama/Llama-2-7b-chat-hf/GPTFuzzer)')
     args = parser.parse_args()
 
-    count_1, count_0, count_err = count_results_in_csv(args)
+    count_1, count_0, count_err, unsafe_list = count_results_in_csv(args)
     total_responses = count_0 + count_1 + count_err
     percentage_safe = (count_0 / total_responses * 100) if total_responses > 0 else 0
+    model_name = args.directory_path.split(os.sep)
+    model_name = os.path.join(model_name[-3], model_name[-2])
+    print(f"Model: {model_name}")
     print(f"Total Responses: {total_responses}")
-    print(f"Count of Safe Responses(0): {count_1}")
-    print(f"Count of Not Safe Responses(1): {count_0}")
+    print(f"Count of Safe Responses(0): {count_0}")
+    print(f"Count of Unsafe Responses(1): {count_1}")
+    unsafe_list.sort()
+    print(f"Unsafe questions： {unsafe_list}")
     print(f"Percentage of Safe Responses: {percentage_safe:.2f}%")
